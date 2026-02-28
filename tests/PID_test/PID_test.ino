7acd78d9390d27b5b45a7c21c102434b65515533
Camleaf <bdaace09@gmail.com>
2026-02-28 14:43:27 -0500

    create test

--- tests/PID_test/PID_test.ino
+++ tests/PID_test/PID_test.ino
@@ -43,9 +43,47 @@ void setup(){
   motor.setDebug(true);
 
   motor.setRPM(0);
+  motor.setDir(true);
+  delay(2000); // Gives A second before jumping into the mainloop
 };
 
 
+/**
+ * The test running is 4 part:
+ * 1. Goes from 0 to max RPM
+ * 2. Goes from max to 50%
+ * 3. Goes from 50% to -Max
+ * 4. Goes back to 0
+ *
+ * 4-second window in between each part
+ */
+
+int mode = 0;
+long lastTime = 0;
+
 void loop(){
   motor.tick();
+  
+  if (millis()-lastTime > 4000){
+      
+
+      switch (mode){
+        case 0:
+            motor.setRPM(kMotorMaxRPM);
+          break;
+        case 1:
+            motor.setRPM(kMotorMaxRPM/2);
+          break;
+        case 2:
+            motor.setDir(false);
+            motor.setRPM(kMotorMaxRPM);
+          break;
+        case 3:
+            motor.setDir(true);
+            motor.setRPM(0);
+          break;
+      }
+      mode += 1;
+      mode %= 4;
+  }
 };
